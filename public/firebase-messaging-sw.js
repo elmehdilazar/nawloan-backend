aad01efc1b9849a8bfde14c6e01c1bf0e2b7b393
elmehdilazar <midomidomido01@gmail.com>
2025-08-25 18:30:59 +0100

    refactor: remove Firebase initialization and messaging logic from app layout

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,6 @@
 importScripts("https://www.gstatic.com/firebasejs/10.12.3/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.3/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDxTycXHWx6hMnpx90fSo2Y8SOFGXomA-w",
-  authDomain: "nawloan-eff12.firebaseapp.com",
-  projectId: "nawloan-eff12",
-  messagingSenderId: "997400731253",
-  appId: "1:997400731253:web:d0ae522e19b8fce924a23c"
-});
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
@@ -25,4 +18,4 @@ messaging.onBackgroundMessage(function(payload) {
   self.clients.matchAll({ type: "window", includeUncontrolled: true }).then(clients => {
     clients.forEach(client => client.postMessage({ __fcm: true, payload }));
   });
-});
+});                                        
