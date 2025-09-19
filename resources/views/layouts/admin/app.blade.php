423c9733be3b33ad610c5e59555c5db1a4dd37f2
elmehdilazar <midomidomido01@gmail.com>
2025-09-19 21:44:27 +0100

    feat: update Firebase initialization and messaging handling in app.blade.php

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -216,59 +216,21 @@
             console.log('Service worker registration failed, error:', err);
         });
     }
-</script>
-<script type="module">
-    // Import the functions you need from the SDKs you need
-   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-
-    // Your web app's Firebase configuration
-    /*const firebaseConfig = {
-    apiKey: "AIzaSyDxTycXHWx6hMnpx90fSo2Y8SOFGXomA-w",
-    authDomain: "nawloan-eff12.firebaseapp.com",
-    projectId: "nawloan-eff12",
-    storageBucket: "nawloan-eff12.appspot.com",
-    messagingSenderId: "997400731253",
-    appId: "1:997400731253:web:d0ae522e19b8fce924a23c"
-    };*/
-    const firebaseConfig = {
+    firebase.initializeApp({
      apiKey: "AIzaSyDxTycXHWx6hMnpx90fSo2Y8SOFGXomA-w",
   authDomain: "nawloan-eff12.firebaseapp.com",
-  databaseURL: "https://nawloan-eff12-default-rtdb.firebaseio.com",
-  projectId: "nawloan-eff12",
-  storageBucket: "nawloan-eff12.firebasestorage.app",
-  messagingSenderId: "997400731253",
+     projectId: "nawloan-eff12",
+ messagingSenderId: "997400731253",
   appId: "1:997400731253:web:d0ae522e19b8fce924a23c",
-  measurementId: "G-8GEL2Y9LVZ"
-    };
-    // Initialize Firebase
-    //  const app = initializeApp(firebaseConfig);
-    firebase.initializeApp(firebaseConfig);
+    });
     const messaging = firebase.messaging();
+     messaging.onMessage((payload) => {  
+      console.log('Foreground message:', payload);
+      alert(payload.notification.body);
+      
 
-    function initFirebaseMessagingRegistration() {
-        messaging.requestPermission().then(function () {
-            return messaging.getToken();
-        }).then(function(token) {
-            axios.post("{{ route('admin.fcmToken') }}",{
-                _method:"PATCH",
-                token
-            }).then(({data})=>{
-                $('#fcmtoken').val(token);
-                console.log('success',data)
-            }).catch(({response:{data}})=>{
-                console.error('errors',data)
-            })
-        }).catch(function (err) {
-            console.log(`Token Error :: ${err}`);
-        });
-    }
-
-    initFirebaseMessagingRegistration();
-
-    messaging.onMessage(function({data:{body,title}}){
-        new Notification(title, {body});
+      writeStatus('Foreground push received.', true);
+      // Optionally show a toast or update UI
     });
 </script>
 
