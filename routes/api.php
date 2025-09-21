291f7d5c4045f8b2635353da8c558a41a43c8150
elmehdilazar <midomidomido01@gmail.com>
2025-09-21 23:59:36 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -347,7 +347,7 @@ Route::get('/processQRCode', [App\Http\Controllers\Clients\QRController::class,'
 use App\Notifications\FcmPushNotification;
 use Illuminate\Support\Facades\Notification;
 Route::get('/test-fcm', function () {
-    $testToken = ' d36nXc5LFWmgDY9JzDEG0r:APA91bF7ksXBlNaoOIKkvCniqm4o7e8r0vZkVIuH-Z79otD1vIaBhtkcyey__zULrSxUCob9tmkBmvqu1fHOiH6OOt2KTla8uS5vENzKycaIAvMgX3EgGa0';
+    $testToken = 'd36nXc5LFWmgDY9JzDEG0r:APA91bF7ksXBlNaoOIKkvCniqm4o7e8r0vZkVIuH-Z79otD1vIaBhtkcyey__zULrSxUCob9tmkBmvqu1fHOiH6OOt2KTla8uS5vENzKycaIAvMgX3EgGa0';
 
     $notification = [
         'title' => 'Test Notification',
