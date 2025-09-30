6edc5658cab803dc545da98a1bf5d7a50070a2e8
elmehdilazar <midomidomido01@gmail.com>
2025-09-30 20:37:11 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*',"https://nawloan.net:6001"],
 
     'allowed_origins_patterns' => [],
 
