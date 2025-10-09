0fac0d332816eecd3f4de9ee25e7cfe725fa5e56
elmehdilazar <midomidomido01@gmail.com>
2025-10-09 14:28:00 +0100

    fix route

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -1506,7 +1506,7 @@ public function registerDriverFromCompany(Request $request, $id)
             'title' => 'site.new_driver_from_company',
             'body' => 'site.new_driver_from_company',
             'target' => 'company driver',
-            'link' => route('admin.driver.edit', ['id' => $driver->id]),
+            'link' => route('admin.drivers.edit', ['id' => $driver->id]),
             'target_id' => $driver->id,
             'sender' => $driver->name,
         ];
