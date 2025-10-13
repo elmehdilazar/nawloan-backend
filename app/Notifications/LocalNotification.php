9c2d75aa024a37278e0b52bea62e187b520ad713
elmehdilazar <midomidomido01@gmail.com>
2025-10-13 13:22:53 +0100

    add

--- app/Notifications/LocalNotification.php
+++ app/Notifications/LocalNotification.php
@@ -22,13 +22,13 @@ class LocalNotification extends Notification
          
        
         return [
-            'title' => $this->data['title'],
+            'title' => $this->data['title'] ?? '',
             'body' => $this->data['body'],
-            'target' => $this->data['target'],
-            'link'=>    $this->data['link'],
-            'object'=>    $this->data['object'],
-            'target_id' => $this->data['target_id'],
-            'user' => $this->data['sender'],
+            'target' => $this->data['target'] ?? '',
+            'link'=>    $this->data['link']??'',
+            'object'=>    $this->data['object']??'',
+            'target_id' => $this->data['target_id']??'',
+            'user' => $this->data['sender']??'',
         ];
     }
 }
