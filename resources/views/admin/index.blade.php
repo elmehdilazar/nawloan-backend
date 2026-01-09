d84e63a2fd267acf833e8ba4fe058fd0607bf0c1
elmehdilazar <midomidomido01@gmail.com>
2026-01-09 03:25:06 +0100

    `Updated translations and labels in admin/index.blade.php`

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -439,7 +439,7 @@
                     </td>
                     <td>
                         <a href="#" data-toggle="modal" data-target="#truckModal_{{ $index }}">
-                            {{ $order->car?->name_en }}
+                            {{ app()->getLocale() == 'ar' ? $order->car?->name_ar : $order->car?->name_en }}
                         </a>
                     </td>
                     <td>
@@ -680,7 +680,7 @@
                         @else
                             <div class="no-offer flex-col-center max-width-80">
                                 <img src="{{asset('assets/images/no-data.png')}}" alt="">
-                                <h4>There are no offers has been found for this order yet</h4>
+                                <h4>@lang('site.no_offers_found_for_order_yet')</h4>
                             </div>
                         @endif
                     </div>
@@ -756,7 +756,7 @@
                                                 <li>
                                                     <img src="{{ asset('assets/images/svgs/map-marker-solid.svg') }}"
                                                         alt="">
-                                                    Sharjah
+                                                    @lang('site.sharjah')
                                                 </li>
                                                 <li>
                                                     <img src="{{ asset('assets/images/svgs/dumbbells.svg') }}"
@@ -790,7 +790,7 @@
                                                 <li>
                                                     <img src="{{ asset('assets/images/svgs/map-marker-solid.svg') }}"
                                                         alt="">
-                                                    Sharjah
+                                                    @lang('site.sharjah')
                                                 </li>
                                                 <li>
                                                     <img src="{{ asset('assets/images/svgs/dumbbells.svg') }}"
@@ -1148,27 +1148,27 @@
                                 <img src="{{asset('assets/images/track-path.png')}}" alt="">
                                 <span class="flex-column">
                                     <span class="from">
-                                        <span id="from">UAE ,Sharjah ,Jaber Bin Abdallah st</span>
+                                        <span id="from">@lang('site.tracking_placeholder_address')</span>
                                     </span>
                                     <span class="to">
-                                        <span id="to" >UAE ,Sharjah ,Jaber Bin Abdallah st</span>
+                                        <span id="to">@lang('site.tracking_placeholder_address')</span>
                                     </span>
                                 </span>
                             </div>
                             <ul class="tracking-informations">
                                 <li>
                                     <img src="{{asset('assets/images/svgs/time-check.svg')}}" alt="">
-                                    Estimated Time:&nbsp;<span id="duration"></span>
+                                    @lang('site.estimated_time'):&nbsp;<span id="duration"></span>
                                 </li>
                                 <li>
                                     <img src="{{asset('assets/images/svgs/road.svg')}}" alt="">
-                                    Road Distance:&nbsp;<span id="distance" ></span>
+                                    @lang('site.road_distance'):&nbsp;<span id="distance" ></span>
                                 </li>
                             </ul>
                         </div>
                         <div id="map" class="map" style="height: 460px; width: 100%;"></div>
                         <div class="flex-center">
-                            <a href="#" class="btn btn-navy shadow-none" data-dismiss="modal" aria-label="Close">Back To Order</a>
+                            <a href="#" class="btn btn-navy shadow-none" data-dismiss="modal" aria-label="Close">@lang('site.back_to_order')</a>
                         </div>
                     </div>
                 </div>
